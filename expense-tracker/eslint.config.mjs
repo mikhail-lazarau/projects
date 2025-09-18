cfcd1a56eb0249c1dab4cf44fd77c1c69a8a14f2
Mikhail.Lazarau <Mikhail.Lazarau@comparethemarket.com>
2025-09-18 21:29:04 +0200

    task-1: setup environment

---
+++ expense-tracker/eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    languageOptions: { globals: globals.node },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...configPrettier.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
