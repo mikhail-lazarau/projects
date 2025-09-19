f0a8d44086ed5148e81fd9227dcd0fe7f9173047
Mikhail.Lazarau <Mikhail.Lazarau@comparethemarket.com>
2025-09-19 12:47:00 +0200

    Implement task-2: create basic rest api

---
+++ expense-tracker/src/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const normalizePort = (val: string) => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+      return val;
+    }
+
+    if (port >= 0) {
+      return port;
+    }
+
+    return false;
+};
+
+const config = {
+  port: normalizePort(process.env.PORT || '8080'),
+  databaseUrl: process.env.DATABASE_URL || '',
+};
+
+export default config;
