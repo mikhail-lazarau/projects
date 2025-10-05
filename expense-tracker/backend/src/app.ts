b40bd9c1dfeb9686218abb16b3d550fc22bc2f42
Mikhail.Lazarau <Mikhail.Lazarau@comparethemarket.com>
2025-10-05 21:52:41 +0200

    husky test

--- expense-tracker/backend/src/app.ts
+++ expense-tracker/backend/src/app.ts
@@ -10,7 +10,11 @@ const app = express();
 app.use(express.json());
 
 const expensesRepository = new ExpensesRepository();
-const expensesService = new ExpensesService(expensesRepository);
+const expensesService = new ExpensesService(
+  expensesRepository)
+  
+  
+  ;
 const expensesController = new ExpensesController(expensesService);
 
 app.use('/api/expenses', expensesController.router);
