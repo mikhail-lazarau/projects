bb8b68eeb39538806410ce3d870a3295b23d000b
Mikhail.Lazarau <Mikhail.Lazarau@comparethemarket.com>
2025-10-06 14:43:18 +0200

    feat(frontend): configure linting and formatting

--- expense-tracker/frontend/eslint.config.mjs
+++ expense-tracker/frontend/eslint.config.mjs
@@ -1,12 +1,38 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettier.rules,
+      'prettier/prettier': 'error',
+    },
+  },
 ];
