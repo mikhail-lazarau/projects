b69bee707dd6050d50c114eaf1d1b239034af7a2
Mikhail.Lazarau <Mikhail.Lazarau@comparethemarket.com>
2025-10-06 16:14:06 +0200

    chore(workspaces): clean up linting and hook configurations

--- expense-tracker/backend/src/app.ts
+++ expense-tracker/backend/src/app.ts
@@ -18,8 +18,6 @@ app.use('/api/expenses', expensesController.router);
 
 app.get('/api/ping', (req: Request, res: Response) => {
   res.json({ message: 'pong' });
-
-  
 });
 
 app.use((req, res) => {
