67a3c273d8278418ecc883905a977f005d1c8d16
Mikhail.Lazarau <Mikhail.Lazarau@comparethemarket.com>
2025-10-14 15:09:04 +0200

    test husky

--- expense-tracker/frontend/src/components/Loader/Loader.tsx
+++ expense-tracker/frontend/src/components/Loader/Loader.tsx
@@ -3,6 +3,7 @@ import styles from './Loader.module.css';
 export const Loader = () => (
 
     
+
   <div className={styles.loaderContainer} data-testid="loader">
     <svg
       className={styles.loader}
